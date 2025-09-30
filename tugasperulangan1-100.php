19889790a5f5c214e97b80353c00d1a893101994
leksaaura <leksaaura@gmail.com>
2025-09-30 13:52:05 +0700

    latihan_php

--- tugasperulangan1-100.php
+++ tugasperulangan1-100.php
@@ -1,13 +1,10 @@
 <?php
-// Program perulangan dari 100 sampai 1000
 for ($i = 100; $i <= 1000; $i++) {
     echo $i . "<br>";
 }
 
-// Penjelasan penggunaan logika perulangan
 echo "<br><b>Logika perulangan dan kontrol banyak digunakan untuk kasus:</b><br>";
 
-// Contoh kasus penggunaan
 echo "1. Pengolahan data (misalnya menghitung total nilai dari sekumpulan data).<br>";
 echo "2. Pencarian & penyaringan data (misalnya mencari data tertentu dalam array atau database).<br>";
 echo "3. Simulasi proses (contoh: simulasi pergerakan objek dalam game).<br>";
